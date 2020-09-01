64a99159cd407152114d219b6f34de0307aa2131
Kysowmya <sowmya.ky@dunnsolutions.com>
2020-09-02 01:13:17 +0530

    added lang3 to pom.xml

--- src/main/java/com/javatpoint/application1/Application.java
+++ src/main/java/com/javatpoint/application1/Application.java
@@ -1,11 +1,15 @@
 package com.javatpoint.application1;
 import java.util.*;
+import org.apache.commons.lang3.StringUtils;
+
 
 public class Application{
 
 public static void main(String[] args){
 System.out.println("try");
-
+String t="sob top fog";
+String[] se=StringUtils.split(t,' ');
+System.out.println(se.length);
 }
 
 }
